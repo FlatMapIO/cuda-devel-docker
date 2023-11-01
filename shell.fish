a64e9b3920334898e8ac63571f5d48a2e4d100f3
huodon <huodon@gmail.com>
2023-11-01 21:49:51 +0800

    WIP

--- shell.fish
+++ shell.fish
@@ -6,6 +6,7 @@ docker run --rm -it \
     -v /etc/apt/sources.list:/etc/apt/sources.list \
     -v $HOME/.cache:/home/vscode/.cache \
     -v $HOME/.config:/home/vscode/.config \
+    -v $HOME/.local:/home/vscode/.local \
     -v $HOME/.vscode-insdiers:/home/vscode/.vscode-insdiers \
     -v $HOME/.codeium:/home/vscode/.codeium \
     -v $PWD:/workspace \
