9cb46aeebf2acfd57460ee877898cfca576f839c
huodon <huodon@gmail.com>
2023-11-01 20:43:42 +0800

    WIP

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # see also https://hub.docker.com/r/nvidia/cuda/tags
-for version in "11.8.0" "12.1.0"; do
+for version in "11.8.0" "12.1.1"; do
     tag="${version}-cudnn8-devel-ubuntu22.04"
     docker build \
         --build-arg TAG=$tag \
