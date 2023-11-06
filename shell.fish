a6504c1c641f1b39cfe97a4ff67318c1d27616c1
huodon <huodon@gmail.com>
2023-11-06 12:45:33 +0800

    WIP

--- shell.fish
+++ shell.fish
@@ -2,13 +2,13 @@
 
 docker run --rm -it \
     --gpus all \
-    --user vscode \
+    --user sa \
     -v /etc/apt/sources.list:/etc/apt/sources.list \
-    -v $HOME/.gitconfig:/home/vscode/.gitconfig \
-    -v $HOME/.cache:/home/vscode/.cache \
-    -v $HOME/.config:/home/vscode/.config \
-    -v $HOME/.local:/home/vscode/.local \
-    -v $HOME/.vscode-insdiers:/home/vscode/.vscode-insdiers \
-    -v $HOME/.codeium:/home/vscode/.codeium \
+    -v $HOME/.gitconfig:/home/sa/.gitconfig \
+    -v $HOME/.cache:/home/sa/.cache \
+    -v $HOME/.config:/home/sa/.config \
+    -v $HOME/.local:/home/sa/.local \
+    -v $HOME/.vscode-insdiers:/home/sa/.vscode-insdiers \
+    -v $HOME/.codeium:/home/sa/.codeium \
     -v $PWD:/workspace \
     huodon/cuda-devel:12.1.1
